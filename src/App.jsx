b6ffffde2a7bc1c73c47fe63dcce5b23c9bd6f2d
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2022-12-21 19:38:16 +0700

    create layout with navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
+    <div className="App min-h-screen max-w-screen">
+      {/* <Login /> */}
+      <Layout />
       {/* <Register /> */}
     </div>
   );
---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React, { Children } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+    return (
+        <div className='bg-white'>
+            <Navbar />
+                {children}
+            <Footer />
+        </div>
+    );
+}
+
+export default Layout;
