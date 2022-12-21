350710476d35f646ed252b9af47c4b3e54022c19
hendrywilliam <hendriwilliam29@gmail.com>
2022-12-21 23:17:40 +0700

    done slicing halaman tambah data dan data diri pasien

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import TambahData from "./pages/TambahData";
+import DataDiriPasien from "./pages/DataDiriPasien";
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      {/* <Register /> */}
+      {/* <Login />
+      <Register /> */}
+      {/* <TambahData /> */}
+      <DataDiriPasien />
     </div>
   );
 }
