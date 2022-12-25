01da0f227c819cb22e6587c2e8a9f22e52ebf5eb
Almira Mahsa <almiramahsa9@gmail.com>
2022-12-25 16:40:08 +0700

    slicing halaman pembayaran-selesai + responsive finish

--- components/BankPayment.jsx
+++ components/BankPayment.jsx
@@ -7,7 +7,7 @@ import { Button, ButtonGroup } from '@chakra-ui/react';
 import { ChevronRightIcon } from '@chakra-ui/icons';
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react';
 import { useDisclosure } from '@chakra-ui/react';
-import Lorem from 'react-lorem-component';
+
 import ModalPayment from './ModalPayment';
 
 function BankPayment() {
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import HomePage from "./pages/HomePage";
-import TambahData from "./pages/TambahData";
-import DataDiriPasien from "./pages/DataDiriPasien";
-import DetailCariRumahSakit from "./pages/DetailCariRumahSakit";
-import Dashboard from "./pages/Admin/Dashboard";
-import DetailDaftarRumahSakit from "./pages/DetailDaftarRumahSakit";
-import Pembayaran from "./pages/Pembayaran";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import HomePage from './pages/HomePage';
+import TambahData from './pages/TambahData';
+import DataDiriPasien from './pages/DataDiriPasien';
+import DetailCariRumahSakit from './pages/DetailCariRumahSakit';
+import Dashboard from './pages/Admin/Dashboard';
+import DetailDaftarRumahSakit from './pages/DetailDaftarRumahSakit';
+import Pembayaran from './pages/Pembayaran';
+import Payment from './pages/Payment';
+import PembayaranSelesai from './pages/PembayaranSelesai';
 
 function App() {
   return (
@@ -28,6 +30,7 @@ function App() {
         {/* <Dashboard /> */}
         {/*<Pembayaran /> */}
         <Route path="/payment" element={<Payment />} />
+        <Route path="pembayaran/selesai" element={<PembayaranSelesai />} />
       </Routes>
     </BrowserRouter>
   );
