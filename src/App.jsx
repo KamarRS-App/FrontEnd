beacc13144a8a02688d42df4600de4f09bb103f7
hendrywilliam <hendriwilliam29@gmail.com>
2022-12-25 22:54:33 +0700

    responsive sudah semua

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-
 import Pembayaran from "./pages/Pembayaran";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/layout";
 import SettingPage from "./pages/Admin/SettingPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import HomePage from "./pages/HomePage";
@@ -15,11 +14,10 @@ import Dashboard from "./pages/Admin/Dashboard";
 import DetailDaftarRumahSakit from "./pages/DetailDaftarRumahSakit";
 import UserPage from "./pages/Admin/UserPage";
 import RoomPage from "./pages/Admin/RoomPage";
-
-import '@fontsource/plus-jakarta-sans/700.css';
-import '@fontsource/plus-jakarta-sans/400.css';
-import '@fontsource/plus-jakarta-sans/500.css';
-import '@fontsource/plus-jakarta-sans/600.css';
+import "@fontsource/plus-jakarta-sans/700.css";
+import "@fontsource/plus-jakarta-sans/400.css";
+import "@fontsource/plus-jakarta-sans/500.css";
+import "@fontsource/plus-jakarta-sans/600.css";
 import theme from "../utils/extendedTheme";
 import "./App.css";
 
@@ -27,9 +25,7 @@ function App() {
   return (
     <BrowserRouter>
       <ChakraProvider theme={theme}>
-        <Box
-          minHeight={'100vh'}
-        >
+        <Box minHeight={"100vh"}>
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
@@ -37,7 +33,10 @@ function App() {
             <Route path="/patient/add" element={<TambahData />} />
             <Route path="/patient" element={<DataDiriPasien />} />
             <Route path="/search/detail" element={<DetailCariRumahSakit />} />
-            <Route path="/detail/patient" element={<DetailDaftarRumahSakit /> } />
+            <Route
+              path="/detail/patient"
+              element={<DetailDaftarRumahSakit />}
+            />
             <Route path="/admin/dashboard" element={<Dashboard />} />
             <Route path="/payment" element={<Pembayaran />} />
             <Route path="/admin/user" element={<UserPage />} />
