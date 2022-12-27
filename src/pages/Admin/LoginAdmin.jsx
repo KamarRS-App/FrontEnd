4417cba34015f463c763b137e580070c92787940
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2022-12-27 10:24:30 +0700

    setup yup validation and hide/show password

--- src/pages/Admin/LoginAdmin.jsx
+++ src/pages/Admin/LoginAdmin.jsx
@@ -1,16 +1,52 @@
-import { Box, Button, Checkbox, Flex, FormControl, FormLabel, HStack, Image, Input, Stack, Text } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import { Box, Button, Checkbox, Flex, FormControl, FormErrorMessage, FormLabel, HStack, Image, Input, Stack, Text } from '@chakra-ui/react';
+import React, { useRef, useState } from 'react';
 import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
+import * as Yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 const LoginAdmin = () => {
     const [showPassword, setShowPassword] = useState(false);
+    const [show, setShow] = useState('');
+
+    const initialValues = {
+        email: '',
+        password: '',
+    }
+
+    const [initialValue, setInitialValue] = useState(initialValues);
+
+    const schema = Yup.object().shape({
+        email: Yup.string().email('Email tidak sesuai').required('Email wajib diisi'),
+        password: Yup.string().required('Password wajib diisi').min(8, 'Password minimal 8 karakter')
+    })
+
+    const { register: loginAdmin, handleSubmit, formState: { errors } } = useForm({
+        mode: 'onTouched',
+        reValidateMode: 'onSubmit',
+        resolver: yupResolver(schema),
+        defaultValues: initialValue,
+    })
+
+    const onError = (error) => {
+        console.log(error)
+    }
+
+    const onSubmit = (values) => {
+        console.log(values)
+    }
+
+    const onShowPassword = (e) => {
+        setShow(e.target.value)
+    }
+
     return (
         <Stack
             minWidth={'full'}
             minHeight={'100vh'}
             maxHeight={'100vh'}
             bg={'#FAFAFA'}
-            p={'10'}
+            p={{ base: '5', md: '10' }}
         >
             <Image
                 height='auto'
@@ -34,94 +70,123 @@ const LoginAdmin = () => {
                     width={'full'}
                     height={'full'}
                     borderColor={'#E0E0E0'}
-                    px={'80px'}
-                    py={'70px'}
+                    px={{ base: '10', md: '80px' }}
+                    py={{ base: '10', md: '70px' }}
                 >
                     <Text
                         color={'#1FA8F6'}
-                        fontSize={'36px'}
+                        fontSize={{ base: '26px', md: '36px' }}
                         fontWeight={'600'}
                     >
                         Login as Admin
                     </Text>
-                    <FormControl
-                        mt={'8'}
-                    >
-                        <FormLabel
-                            fontSize={'14px'}
-                            color={'#695C5C'}
+                    <form onSubmit={handleSubmit(onSubmit, onError)}>
+                        <FormControl
+                            mt={'8'}
+                            isInvalid={errors.email}
                         >
-                            Email
-                        </FormLabel>
-                        <Input placeholder='email@mail.com' id="email" type='email' border={'1px'} borderColor={'#00000066'} height={'50px'} borderRadius={'xl'} _placeholder={{ color: '#000000B2' }} />
-                        {/* {errors.name && <FormErrorMessage>{errors.name.message}</FormErrorMessage>} */}
-                    </FormControl>
-                    <FormControl
-                        mt={'5'}
-                    >
-                        <FormLabel
-                            fontSize={'14px'}
-                            color={'#695C5C'}
+                            <FormLabel
+                                fontSize={'14px'}
+                                color={'#695C5C'}
+                            >
+                                Email
+                            </FormLabel>
+                            <Input
+                                placeholder='email@mail.com'
+                                id="email"
+                                type='email'
+                                border={'1px'}
+                                borderColor={'#00000066'}
+                                height={'50px'}
+                                borderRadius={'xl'}
+                                _placeholder={{ color: '#000000B2' }}
+                                {...loginAdmin('email')}
+                            />
+                            {errors.email && <FormErrorMessage>{errors.email.message}</FormErrorMessage>}
+                        </FormControl>
+                        <FormControl
+                            mt={'5'}
+                            isInvalid={errors.password}
                         >
-                            Password
-                        </FormLabel>
-                        <Stack
-                            position={'relative'}
+                            <FormLabel
+                                fontSize={'14px'}
+                                color={'#695C5C'}
+                            >
+                                Password
+                            </FormLabel>
+                            <Stack
+                                position={'relative'}
+                            >
+                                <Input
+                                    placeholder='password'
+                                    id="password"
+                                    type={showPassword ? 'text' : 'password'}
+                                    border={'1px'}
+                                    borderColor={'#00000066'}
+                                    height={'50px'}
+                                    borderRadius={'xl'}
+                                    _placeholder={{ color: '#000000B2' }}
+                                    onInput={onShowPassword}
+                                    {...loginAdmin('password')}
+                                />
+                                {
+                                    show &&
+                                    <Box
+                                        position={'absolute'}
+                                        right={'4'}
+                                        bottom={'4'}
+                                        cursor={'pointer'}
+                                        onClick={() => setShowPassword(!showPassword)}
+                                    >
+                                        {
+                                            showPassword ? <BsFillEyeSlashFill /> : <BsFillEyeFill />
+                                        }
+                                    </Box>
+                                }
+                                {errors.password && <FormErrorMessage>{errors.password.message}</FormErrorMessage>}
+                            </Stack>
+                        </FormControl>
+                        <Flex
+                            mt={'8'}
+                            justify={'space-between'}
                         >
-                            <Input placeholder='password' id="password" type='password' border={'1px'} borderColor={'#00000066'} height={'50px'} borderRadius={'xl'} _placeholder={{ color: '#000000B2' }} />
-                            <Box
-                                position={'absolute'}
-                                right={'4'}
-                                bottom={'4'}
-                                cursor={'pointer'}
-                                onClick={() => setShowPassword(!showPassword)}
+                            <Checkbox
+                                colorScheme={'linkedin'}
+                                size='lg'
 
                             >
-                                {showPassword ?  <BsFillEyeSlashFill /> : <BsFillEyeFill />}
-                            </Box>
-                        </Stack>
-
-                        {/* {errors.name && <FormErrorMessage>{errors.name.message}</FormErrorMessage>} */}
-                    </FormControl>
-                    <Flex
-                        mt={'8'}
-                        justify={'space-between'}
-                    >
-                        <Checkbox
-                            colorScheme={'linkedin'}
-                            size='lg'
-
-                        >
+                                <Text
+                                    fontWeight={'600'}
+                                    fontSize={'15px'}
+                                >
+                                    Remember Me
+                                </Text>
+                            </Checkbox>
                             <Text
-                                fontWeight={'600'}
+                                color={'#E86969'}
+                                fontWeight={'500'}
                                 fontSize={'15px'}
                             >
-                                Remember Me
+                                lupa password
                             </Text>
-                        </Checkbox>
-                        <Text
-                            color={'#E86969'}
-                            fontWeight={'500'}
-                            fontSize={'15px'}
+                        </Flex>
+                        <Button
+                            type='submit'
+                            mt={'10'}
+                            width={'full'}
+                            bg={'#1FA8F6'}
+                            color={'white'}
+                            fontSize={'16px'}
+                            fontWeight={'600'}
+                            _hover={{ bg: '#3AB8FF' }}
                         >
-                            lupa password
-                        </Text>
-                    </Flex>
-                    <Button
-                        mt={'10'}
-                        width={'full'}
-                        bg={'#1FA8F6'}
-                        color={'white'}
-                        fontSize={'16px'}
-                        fontWeight={'600'}
-                        _hover={{ bg: '#3AB8FF' }}
-                    >
-                        Login
-                    </Button>
+                            Login
+                        </Button>
+                    </form>
                 </Box>
-
             </HStack>
-        </Stack>
+
+        </Stack >
     );
 }
 
