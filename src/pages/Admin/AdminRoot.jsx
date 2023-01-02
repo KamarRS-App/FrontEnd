a7f1006ab16f6e7666dbee2d75cfbd0e5e6ae669
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2023-01-02 08:21:36 +0700

    fix error merge

--- src/pages/Admin/AdminRoot.jsx
+++ src/pages/Admin/AdminRoot.jsx
@@ -8,9 +8,9 @@ import { MdModeEdit, MdOutlineDeleteOutline } from "react-icons/md";
 import PopupAdmin from "../../components/PopupAdmin";
 import { useDisclosure } from "@chakra-ui/hooks";
 import {
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
 } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
 import { Select } from "@chakra-ui/select";
@@ -25,105 +25,78 @@ import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
 import PopupDelete from "../../components/PopupDelete";
 
 const AdminRoot = () => {
-  const {
-    isOpen: isModalCreateOpen,
-    onOpen: onModalCreateOpen,
-    onClose: onCloseModalCreate,
-  } = useDisclosure();
-  const {
-    isOpen: isModalEditOpen,
-    onOpen: onModalEditOpen,
-    onClose: onCloseModalEdit,
-  } = useDisclosure();
-  const {
-    isOpen: isModalDeleteOpen,
-    onOpen: onModalDeleteOpen,
-    onClose: onCloseModalDelete,
-  } = useDisclosure();
+    const { isOpen: isModalCreateOpen, onOpen: onModalCreateOpen, onClose: onCloseModalCreate, } = useDisclosure();
+    const { isOpen: isModalEditOpen, onOpen: onModalEditOpen, onClose: onCloseModalEdit, } = useDisclosure();
+    const { isOpen: isModalDeleteOpen, onOpen: onModalDeleteOpen, onClose: onCloseModalDelete, } = useDisclosure();
 
-  const role = Cookies.get("role");
-  const token = Cookies.get("token");
-  const navigate = useNavigate();
-  const toast = useToast();
-  const [adminStaff, setAdminStaff] = useState([]);
-  const [hospitals, setHospitals] = useState([]);
-  const [showPassword, setShowPassword] = useState(false);
-  const [show, setShow] = useState("");
-  const [adminId, setAdminId] = useState("");
+    const role = Cookies.get("role");
+    const token = Cookies.get("token");
+    const navigate = useNavigate();
+    const toast = useToast();
+    const [adminStaff, setAdminStaff] = useState([]);
+    const [hospitals, setHospitals] = useState([]);
+    const [showPassword, setShowPassword] = useState(false);
+    const [show, setShow] = useState("");
+    const [adminId, setAdminId] = useState("");
 
-  const initialValues = {
-    nama: "",
-    email: "",
-    kata_sandi: "",
-    peran: "Admin",
-    hospital_id: null,
-  };
+    const initialValues = {
+        nama: "",
+        email: "",
+        kata_sandi: "",
+        peran: "Admin",
+        hospital_id: null,
+    };
 
-  const initialValuesUpdate = {
-    nama: "",
-    email: "",
-    peran: "Admin",
-    hospital_id: null,
-  };
+    const initialValuesUpdate = {
+        nama: "",
+        email: "",
+        peran: "Admin",
+        hospital_id: null,
+    };
 
-  const [initialValue, setInitialValue] = useState(initialValues);
-  const [initialValueUpdate, setInitialValueUpdate] = useState(initialValuesUpdate);
+    const [initialValue, setInitialValue] = useState(initialValues);
+    const [initialValueUpdate, setInitialValueUpdate] = useState(initialValuesUpdate);
 
-  const schema = Yup.object().shape({
-    nama: Yup.string().required("Nama tidak boleh kosong"),
-    email: Yup.string()
-      .email("Format Email salah")
-      .required("Email tidak boleh kosong"),
-    kata_sandi: Yup.string()
-      .min(6, "Password tidak boleh kurang dari 6 karakter")
-      .max(32, "Password tidak boleh lebih dari 32 karakter")
-      .required("Password tidak boleh kosong"),
-    hospital_id: Yup.number().required(
-      "Rumah Sakit Bekerja tidak boleh kosong"
-    ),
-  });
-
-  const schemaUpdate = Yup.object().shape({
-    nama: Yup.string().required("Nama tidak boleh kosong"),
-    email: Yup.string()
-      .email("Format Email salah")
-      .required("Email tidak boleh kosong"),
-    hospital_id: Yup.number().required(
-      "Rumah Sakit Bekerja tidak boleh kosong"
-    ),
-  });
-
-  const {
-    register: createAdmin,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    mode: "onTouched",
-    reValidateMode: "onSubmit",
-    resolver: yupResolver(schema),
-    defaultValues: initialValue,
-  });
-
-  const {
-    register: updateAdminFunc,
-    handleSubmit: handleUpdateAdmin,
-    setValue: setValueUpdate,
-    formState: { errors: errorsUpdate },
-  } = useForm({
-    mode: "onTouched",
-    reValidateMode: "onSubmit",
-    resolver: yupResolver(schemaUpdate),
-    defaultValues: initialValueUpdate,
-  });
-
-    const { register: createAdmin, handleSubmit, setValue: setValueCreate, formState: { errors } } = useForm({
-        mode: "onTouched",
-        reValidateMode: "onSubmit",
-        resolver: yupResolver(schema),
-        defaultValues: initialValue,
+    const schema = Yup.object().shape({
+        nama: Yup.string().required("Nama tidak boleh kosong"),
+        email: Yup.string()
+            .email("Format Email salah")
+            .required("Email tidak boleh kosong"),
+        kata_sandi: Yup.string()
+            .min(6, "Password tidak boleh kurang dari 6 karakter")
+            .max(32, "Password tidak boleh lebih dari 32 karakter")
+            .required("Password tidak boleh kosong"),
+        hospital_id: Yup.number().required(
+            "Rumah Sakit Bekerja tidak boleh kosong"
+        ),
     });
 
-  const getAllAdminStaff = async () => {
+    const schemaUpdate = Yup.object().shape({
+        nama: Yup.string().required("Nama tidak boleh kosong"),
+        email: Yup.string()
+            .email("Format Email salah")
+            .required("Email tidak boleh kosong"),
+        hospital_id: Yup.number().required(
+            "Rumah Sakit Bekerja tidak boleh kosong"
+        ),
+    });
+
+    const {
+        register: createAdmin, handleSubmit, formState: { errors }, } = useForm({
+            mode: "onTouched",
+            reValidateMode: "onSubmit",
+            resolver: yupResolver(schema),
+            defaultValues: initialValue,
+        });
+
+    const { register: updateAdminFunc, handleSubmit: handleUpdateAdmin, setValue: setValueUpdate, formState: { errors: errorsUpdate } } = useForm({
+        mode: "onTouched",
+        reValidateMode: "onSubmit",
+        resolver: yupResolver(schemaUpdate),
+        defaultValues: initialValueUpdate,
+    });
+
+    const getAllAdminStaff = async () => {
         await api.getAdmin(token)
             .then(response => {
                 const data = response.data.data;
