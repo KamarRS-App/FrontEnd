fc671574028e3dbd2f1285175d8b799562d0f797
hendrywilliam <hendriwilliam29@gmail.com>
2023-01-04 21:34:57 +0700

    minor update, melakukan beberapa penyesuaian

--- src/components/HeadAdmin.jsx
+++ src/components/HeadAdmin.jsx
@@ -1,64 +1,57 @@
-import { Button, ButtonGroup } from '@chakra-ui/button';
-import { Box, Flex, Stack, Text } from '@chakra-ui/layout';
-import React from 'react';
-import { CiFilter, CiSearch } from 'react-icons/ci';
-import { IoAddOutline } from 'react-icons/io5';
+import { Button, ButtonGroup } from "@chakra-ui/button";
+import { Box, Flex, Stack, Text } from "@chakra-ui/layout";
+import React from "react";
+import { CiFilter, CiSearch } from "react-icons/ci";
+import { IoAddOutline } from "react-icons/io5";
 
-const HeadAdmin = ({ title, isAdd }) => {
-    return (
-        <Flex
-            bg='white'
-            p={{ base:'2', sm:'5', md:'10' }}
-            wrap='wrap'
-            justifyContent={{ base:'center', sm:'space-between' }}
-        >
-            <Text
-                fontSize={'20px'}
-                fontWeight={'600'}
-                color={'#1FA8F6'}
-            >
-                {title}
-            </Text>
-            <Flex
-                color={'#333333'}
-                gap='5'
-                pt={{ base:'5', sm:'0' }}
-                width={{ base:'100%', sm:'auto' }}
-                justifyContent={'center'}
-            >
-                <ButtonGroup>
-                    <Button
-                        onClick={isAdd}
-                        bg='transparent'
-                        border='1px'
-                        borderColor={'#E0E0E0'}
-                    >
-                        <IoAddOutline
-                            fontSize={'30px'}
-                        />
-                    </Button>
-                    <Button
-                        bg='transparent'
-                        border='1px'
-                        borderColor={'#E0E0E0'}
-                    >
-                        <CiSearch
-                            fontSize={'30px'}
-                        />
-                    </Button>
-                    <Button
-                        bg='transparent'
-                        border='1px'
-                        borderColor={'#E0E0E0'}
-                    >
-                        <CiFilter
-                            fontSize={'32px'}
-                        />
-                    </Button>
-                </ButtonGroup>
-            </Flex>
-        </Flex>
-    );
-}
+const HeadAdmin = ({ title, isAdd, showAdd, showSearch, showFilter }) => {
+  return (
+    <Flex
+      bg="white"
+      p={{ base: "2", sm: "5", md: "10" }}
+      wrap="wrap"
+      justifyContent={{ base: "center", sm: "space-between" }}
+    >
+      <Text fontSize={"20px"} fontWeight={"600"} color={"#1FA8F6"}>
+        {title}
+      </Text>
+      <Flex
+        color={"#333333"}
+        gap="5"
+        pt={{ base: "5", sm: "0" }}
+        width={{ base: "100%", sm: "auto" }}
+        justifyContent={"center"}
+      >
+        <ButtonGroup>
+          <Button
+            onClick={isAdd}
+            bg="transparent"
+            border="1px"
+            borderColor={"#E0E0E0"}
+            display={showAdd}
+          >
+            <IoAddOutline fontSize={"30px"} />
+          </Button>
+          <Button
+            bg="transparent"
+            border="1px"
+            borderColor={"#E0E0E0"}
+            display={showSearch}
+          >
+            <CiSearch fontSize={"30px"} />
+          </Button>
+          <Button
+            bg="transparent"
+            border="1px"
+            borderColor={"#E0E0E0"}
+            display={showFilter}
+          >
+            <CiFilter fontSize={"32px"} />
+          </Button>
+        </ButtonGroup>
+      </Flex>
+    </Flex>
+  );
+};
 
 export default HeadAdmin;
